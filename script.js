9b2767a49619478696cd3cd0bdbb5fd6760c3bf9
camiladdev <ddevcamila@gmail.com>
2025-01-16 13:54:05 -0300

    Update script.js
    anexando service worker

--- script.js
+++ script.js
@@ -48,3 +48,10 @@ document.addEventListener("DOMContentLoaded", function () {
         themeToggle.textContent = isDarkMode ? "☀️ Modo Claro" : "🌙 Modo Escuro";
     });
 });
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js")
+    .then(() => console.log("Service Worker registrado com sucesso!"))
+    .catch(err => console.log("Erro ao registrar Service Worker:", err));
+}
+
