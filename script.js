d30a7084b118c1c4a19b7c3c21d8c4dc353f7538
camiladdev <ddevcamila@gmail.com>
2025-01-15 21:15:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Seleciona os elementos do HTML
+
+const dice = document.getElementById("dice");
+const rollButton = document.getElementById("rollButton");
+
+// Função para simular a rolagem de um dado de 20 lados
+
+function rollDice() {
+    rollButton.disabled = true;
+
+    let counter = 0;
+    const interval = setInterval(() => {
+        const randomFace = Math.floor(Math.random() * 20) + 1;
+        dice.textContent = randomFace;
+
+        counter++;
+        if (counter === 10) {
+            clearInterval(interval);
+            const finalFace = Math.floor(Math.random() * 20) + 1;
+            dice.textContent = finalFace;
+            rollButton.disabled = false;
+        }
+    }, 100);
+}
+
+rollButton.addEventListener("click", rollDice);
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./service-worker.js")
+      .then(() => console.log("Service Worker registrado com sucesso!"))
+      .catch((error) => console.log("Falha ao registrar o Service Worker:", error));
+  }
+  
