6fe23cb25d5f44a0b72cb4db1c67be13ad8363ba
camiladdev <ddevcamila@gmail.com>
2025-01-16 13:23:42 -0300

    Update script.js
    sem pwa

--- script.js
+++ script.js
@@ -35,9 +35,3 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./service-worker.js")
-      .then(() => console.log("Service Worker registrado com sucesso!"))
-      .catch((error) => console.log("Falha ao registrar o Service Worker:", error));
-  }
-  
